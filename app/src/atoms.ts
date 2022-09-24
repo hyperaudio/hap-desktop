9e7ae256eaf04ac3ea369a65d2da1e13f4e1c436
piotrfonte <dev@piotrf.com>
2022-09-24 19:33:23 +0200

    Add MUI w/ support for dark/light mode.

--- app/src/atoms.ts
+++ app/src/atoms.ts
@@ -1,3 +1,7 @@
+// Get system theme
+export const darkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
 import { atomWithStorage } from 'jotai/utils';
 
 export const countAtom = atomWithStorage('counter', 0);
+export const themeAtom = atomWithStorage('theme', darkMode ? 'dark' : 'light');
