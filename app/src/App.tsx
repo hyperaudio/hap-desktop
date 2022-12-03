a91f2e37e97bef8e3296fd2eeb475b9a8e0b0258
piotrfonte <dev@piotrf.com>
2022-12-03 13:26:31 +0100

    Remove obsolete atoms files and styles.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,45 +1,30 @@
 import { useAtom } from 'jotai';
 import { Link } from 'react-router-dom';
 
-import { countAtom, themeAtom } from './atoms';
-
 import { Button, CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 
 import darkTheme from './themes/darkTheme';
 import lightTheme from './themes/lightTheme';
 
-import styles from 'styles/app.module.scss';
+import { SettingsAtom } from './state';
 
 const App: React.FC = () => {
-  // const [count, setCount] = useState(0);
-  const [count, setCount] = useAtom(countAtom);
-  const [theme, setTheme] = useAtom(themeAtom);
+  const [settings] = useAtom(SettingsAtom);
+
+  console.log('WHAT?', { settings });
 
   return (
-    <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
+    <ThemeProvider theme={settings.theme === 'dark' ? darkTheme : lightTheme}>
       <CssBaseline />
-      <Button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>Hello</Button>
-      <div className={styles.app}>
-        <header className={styles.appHeader}>
-          <p>Hello Electron + Vite + React!</p>
-          <p>
-            <button onClick={() => setCount(count => count + 1)}>count is: {count}</button>
-          </p>
+      <div>
+        <header>
           <p>
             <Link to="/settings">Settings</Link>
           </p>
           <p>
             <Link to="/edit">Edit</Link>
           </p>
-          <p>
-            Edit <code>App.tsx</code> and save to test HMR updates.
-          </p>
-          <div>
-            <div className={styles.staticPublic}>
-              Place static files into the <code>/public</code> folder
-            </div>
-          </div>
         </header>
       </div>
     </ThemeProvider>
--- app/src/atoms.ts
+++
@@ -1,7 +0,0 @@
-// Get system theme
-export const darkMode = window.matchMedia('(prefers-color-scheme: dark)');
-
-import { atomWithStorage } from 'jotai/utils';
-
-export const countAtom = atomWithStorage('counter', 0);
-export const themeAtom = atomWithStorage('theme', darkMode ? 'dark' : 'light');
