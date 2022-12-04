c201e09fc9dc858ac22d5cb612ddb533df6f4696
Laurian Gridinoc <laurian@gridinoc.name>
2022-12-04 17:29:08 +0900

    I don't get MUI/styled

--- app/src/components/player/Player.tsx
+++ app/src/components/player/Player.tsx
@@ -1,18 +1,70 @@
-import React, { useMemo, useCallback, useRef, MutableRefObject } from 'react';
+import React, { useMemo, useCallback, useRef, MutableRefObject, useState } from 'react';
 import ReactPlayer from 'react-player';
+import TC from 'smpte-timecode';
+
+import Box from '@mui/material/Box';
+import FastForwardIcon from '@mui/icons-material/FastForward';
+import FastRewindIcon from '@mui/icons-material/FastRewind';
+import Grow from '@mui/material/Grow';
+import IconButton from '@mui/material/IconButton';
+import PauseIcon from '@mui/icons-material/Pause';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import Slider from '@mui/material/Slider';
+import Stack from '@mui/material/Stack';
+import Tooltip from '@mui/material/Tooltip';
+import UnfoldLessIcon from '@mui/icons-material/UnfoldLess';
+import UnfoldMoreIcon from '@mui/icons-material/UnfoldMore';
+import { styled } from '@mui/material/styles';
+
+const PREFIX = 'EditorPage';
+const CONTROLS_HEIGHT = 60;
+
+const classes = {
+  controls: `${PREFIX}-controls`,
+  editor: `${PREFIX}-editor`,
+  paneTitle: `${PREFIX}-paneTitle`,
+  player: `${PREFIX}-player`,
+  root: `${PREFIX}-root`,
+  stage: `${PREFIX}-stage`,
+  theatre: `${PREFIX}-theatre`,
+  toolbar: `${PREFIX}-toolbar`,
+  transcript: `${PREFIX}-transcript`,
+};
 
 const Player = ({
   url,
   playing,
   play,
   pause,
+  time,
   setTime,
+  duration,
+  setDuration,
+  pip,
+  setPip,
+  buffering,
+  setBuffering,
+  hideVideo,
+  setHideVideo,
+  seekTime,
+  setSeekTime,
 }: {
   url: string | null;
   playing: boolean;
   play: () => void;
   pause: () => void;
+  time: number;
   setTime: (t: number) => void;
+  duration: number;
+  setDuration: (d: number) => void;
+  pip: boolean;
+  setPip: (p: boolean) => void;
+  buffering: boolean;
+  setBuffering: (b: boolean) => void;
+  hideVideo: boolean;
+  setHideVideo: (h: boolean) => void;
+  seekTime: number;
+  setSeekTime: (s: number) => void;
 }): JSX.Element | null => {
   const ref = useRef<ReactPlayer>() as MutableRefObject<ReactPlayer>;
 
@@ -22,41 +74,113 @@ const Player = ({
       forceVideo: true,
       file: {
         attributes: {
-          // poster: 'https://via.placeholder.com/720x576.png?text=4:3',
+          // poster: 'https://via.placeholder.com/720x576.png?text=4:3', // TODO
           controlsList: 'nodownload',
         },
-        // hlsOptions: {
-        //   backBufferLength: 30,
-        //   maxMaxBufferLength: 30,
-        // },
       },
     }),
     [],
   );
 
-  const onDuration = useCallback((duration: number) => {
-    console.log({ duration });
-  }, []);
+  const onDuration = useCallback((duration: number) => setDuration(duration), []);
+  const onProgress = useCallback(({ playedSeconds }: { playedSeconds: number }) => setTime(playedSeconds), [setTime]);
+  const onEnablePIP = useCallback(() => setPip(true), []);
+  const onDisablePIP = useCallback(() => setPip(false), []);
+  const onBuffer = useCallback(() => setBuffering(true), []);
+  const onBufferEnd = useCallback(() => setBuffering(false), []);
 
-  const onProgress = useCallback(
-    ({ playedSeconds }: { playedSeconds: number }) => {
-      console.log({ playedSeconds });
-      setTime(playedSeconds);
+  const handleSliderChange = useCallback(
+    (event: any, value: number) => {
+      setSeekTime(value);
+      if (ref.current) ref.current.seekTo(value, 'seconds');
     },
-    [setTime],
+    [ref],
   );
 
+  const toggleVideo = useCallback(() => setHideVideo(!hideVideo), [setHideVideo, hideVideo]);
+
   return url ? (
-    <ReactPlayer
-      controls
-      {...{ ref, url, config, playing, onDuration, onProgress }}
-      onPlay={play}
-      onPause={pause}
-      progressInterval={100}
-      width="100%"
-      height="100%"
-    />
+    <Box className={classes.stage} onClick={playing === true ? pause : play}>
+      {!hideVideo && !pip && (
+        <svg width="100%" viewBox={`0 0 16 9`} fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect width={16} height={9} />
+        </svg>
+      )}
+      <Grow in={!hideVideo || pip}>
+        <Box className={classes.playerWrapper}>
+          <ReactPlayer
+            className={classes.player}
+            config={config}
+            height="100%"
+            onBuffer={onBuffer}
+            onBufferEnd={onBufferEnd}
+            onDisablePIP={onDisablePIP}
+            onDuration={onDuration}
+            onEnablePIP={onEnablePIP}
+            onPlay={play}
+            onProgress={onProgress}
+            playing={playing}
+            progressInterval={100}
+            ref={ref}
+            style={{ lineHeight: 0, visibility: hideVideo || pip ? 'hidden' : 'visible' }}
+            url={url}
+            width="100%"
+          />
+        </Box>
+      </Grow>
+      <Box
+        className={classes.controls}
+        sx={{
+          opacity: { md: playing ? (hideVideo || pip ? 1 : 0) : 1 },
+          pointerEvents: { md: playing ? (hideVideo || pip ? 'all' : 'none') : 'all' },
+          position: hideVideo || pip ? 'static' : 'absolute',
+        }}
+        onClick={e => e.stopPropagation()}
+      >
+        <Stack spacing={2} direction="row" sx={{ alignItems: 'center', width: '100%' }}>
+          {buffering && seekTime !== time ? (
+            <IconButton onClick={pause} color="inherit">
+              {seekTime - time > 0 ? <FastForwardIcon /> : <FastRewindIcon />}
+            </IconButton>
+          ) : playing ? (
+            <IconButton onClick={pause} color="inherit">
+              <PauseIcon />
+            </IconButton>
+          ) : (
+            <IconButton onClick={play} color="inherit">
+              <PlayArrowIcon />
+            </IconButton>
+          )}
+          <Slider
+            aria-label="timeline"
+            defaultValue={0}
+            max={duration}
+            min={0}
+            onChange={handleSliderChange}
+            size="small"
+            sx={{ color: 'white' }}
+            value={time}
+            valueLabelDisplay="auto"
+            valueLabelFormat={timecode}
+          />
+          <Tooltip title={`${hideVideo ? 'Show' : 'Hide'} video`}>
+            <span>
+              <IconButton disabled={pip} onClick={toggleVideo} color="inherit">
+                {hideVideo ? <UnfoldMoreIcon /> : <UnfoldLessIcon />}
+              </IconButton>
+            </span>
+          </Tooltip>
+        </Stack>
+      </Box>
+    </Box>
   ) : null;
 };
+
+const timecode = (seconds: number, frameRate = 25, dropFrame = false) =>
+  TC(seconds * 25, 25, false)
+    .toString()
+    .split(':')
+    .slice(0, 3)
+    .join(':');
 
 export default Player;
--- app/src/pages/Edit.tsx
+++ app/src/pages/Edit.tsx
@@ -15,11 +15,119 @@ import React, { useState, useMemo, useRef, useCallback, useEffect, EffectCallbac
 import { Container, unstable_composeClasses } from '@mui/material';
 import { EditorState, ContentState, RawDraftContentBlock, convertFromRaw } from 'draft-js';
 import { v4 as uuidv4 } from 'uuid';
+import { styled } from '@mui/material/styles';
 
 import { Editor, createEntityMap } from '../components/editor';
 import Player from '../components/player/Player';
 import { StatusBar } from '../modules';
 import { MainView } from '../views';
+
+const PREFIX = 'EditorPage';
+const CONTROLS_HEIGHT = 60;
+
+const classes = {
+  controls: `${PREFIX}-controls`,
+  editor: `${PREFIX}-editor`,
+  paneTitle: `${PREFIX}-paneTitle`,
+  player: `${PREFIX}-player`,
+  root: `${PREFIX}-root`,
+  stage: `${PREFIX}-stage`,
+  theatre: `${PREFIX}-theatre`,
+  toolbar: `${PREFIX}-toolbar`,
+  transcript: `${PREFIX}-transcript`,
+};
+
+const Root = styled('div', {
+  // shouldForwardProp: (prop: any) => prop !== 'isActive',
+})(({ theme }) => ({
+  bottom: 0,
+  left: 0,
+  position: 'fixed',
+  right: 0,
+  top: 0,
+  [`& .${classes.editor}`]: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    zIndex: 1,
+  },
+  [`& .${classes.toolbar}`]: {
+    background: 'black',
+    color: theme.palette.primary.contrastText,
+    zIndex: 1,
+    '& .Mui-disabled': { color: 'rgba(255,255,255,0.5) !important' },
+  },
+  [`& .${classes.theatre}`]: {
+    aligenItems: 'center',
+    transition: `flex-basis ${theme.transitions.duration.standard}ms ${theme.transitions.easing.easeInOut}`,
+    backgroundColor: 'black',
+    color: theme.palette.primary.contrastText,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    width: '100%',
+    '& .Mui-disabled': { color: 'rgba(255,255,255,0.5) !important' },
+    [theme.breakpoints.up('sm')]: {
+      padding: theme.spacing(0, 0, 2),
+    },
+  },
+  [`& .${classes.stage}`]: {
+    borderRadius: theme.shape.borderRadius * 2,
+    lineHeight: 0,
+    position: 'relative',
+    [theme.breakpoints.up('sm')]: {
+      border: `1px solid rgba(255,255,255,0.22)`,
+    },
+  },
+  [`& .${classes.playerWrapper}`]: {
+    height: '100%',
+    paddingTop: '56.25%',
+    position: 'relative',
+    width: '100%',
+  },
+  [`& .${classes.player}`]: {
+    borderRadius: theme.shape.borderRadius * 2,
+    cursor: 'pointer',
+    left: '0',
+    overflow: 'hidden',
+    position: 'absolute',
+    top: '0',
+  },
+  [`& .${classes.controls}`]: {
+    alignItems: 'center',
+    background: `linear-gradient(to top, rgba(0,0,0,1), rgba(0,0,0,0.0))`,
+    borderRadius: theme.shape.borderRadius * 2,
+    bottom: 0,
+    display: 'flex',
+    height: `${CONTROLS_HEIGHT}px`,
+    left: 0,
+    padding: theme.spacing(1),
+    right: 0,
+    transition: `opacity ${theme.transitions.duration.short}ms`,
+  },
+  [`& .${classes.stage}:hover .${classes.controls}`]: {
+    [theme.breakpoints.up('md')]: {
+      opacity: '1 !important',
+      pointerEvents: 'all !important',
+    },
+  },
+  [`& .${classes.transcript}`]: {
+    flexBasis: '60%',
+    flexGrow: 1,
+    width: '100%',
+  },
+  [`& .${classes.paneTitle}`]: {
+    background: theme.palette.divider,
+    color: theme.palette.text.primary,
+    fontSize: '10px',
+    fontWeight: '500',
+    padding: theme.spacing(0, 0.5),
+    position: 'absolute',
+    top: 0,
+    transition: `opacity ${theme.transitions.duration.standard}ms`,
+    zIndex: 1,
+  },
+}));
 
 export const EditPage: React.FC = () => {
   const [loading, setLoading] = useState(false);
@@ -48,15 +156,21 @@ export const EditPage: React.FC = () => {
     contentState: ContentState;
   }>();
 
-  const [time, setTime] = useState(0);
+  const [hideVideo, setHideVideo] = useState(false);
 
-  const noKaraoke = false; // FIXME
+  const [time, setTime] = useState(0);
+  const [seekTime, setSeekTime] = useState(0);
+  const [playing, setPlaying] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [buffering, setBuffering] = useState(false);
+  const [pip, setPip] = useState(false);
+
+  const play = useCallback(() => setPlaying(true), []);
+  const pause = useCallback(() => setPlaying(false), []);
+
   const seekTo = (time: number): void => {
     console.log('TODO seekTo', time);
   };
-  const [playing, setPlaying] = useState(false);
-  const play = useCallback(() => setPlaying(true), []);
-  const pause = useCallback(() => setPlaying(false), []);
 
   const handleSave = useCallback(async () => {
     // if (!blocks || blocks.length === 0) return;
@@ -160,28 +274,55 @@ export const EditPage: React.FC = () => {
     return (() => ipcRenderer.removeAllListeners('menu-action')) as unknown as void; // FIXME
   }, []);
 
+  const noKaraoke = false;
+
   return (
-    <Container maxWidth="md">
-      <button onClick={handleOpen} disabled={loading}>
-        {loading ? 'Opening…' : 'Open'}
-      </button>
-      <button onClick={handleSave} disabled={saving}>
-        {saving ? 'Saving…' : 'Save'}
-      </button>
-      {url ? <Player {...{ url, playing, play, pause, setTime }} /> : null}
-      {initialState ? (
-        <Editor
-          {...{ initialState, time, seekTo, speakers, setSpeakers, playing, play, pause }}
-          autoScroll
-          onChange={setDraft}
-          playheadDecorator={noKaraoke ? null : undefined}
-        />
-      ) : error ? (
-        <p>Error: {error?.message}</p>
-      ) : (
-        <p>{loading ? 'opening file / skeleton' : 'no file, please open one'}</p>
-      )}
-    </Container>
+    <Root className={classes.root}>
+      <Container maxWidth="md">
+        <button onClick={handleOpen} disabled={loading}>
+          {loading ? 'Opening…' : 'Open'}
+        </button>
+        <button onClick={handleSave} disabled={saving}>
+          {saving ? 'Saving…' : 'Save'}
+        </button>
+        <hr />
+        {url ? (
+          <Player
+            {...{
+              url,
+              playing,
+              play,
+              pause,
+              buffering,
+              setBuffering,
+              time,
+              setTime,
+              duration,
+              setDuration,
+              pip,
+              setPip,
+              hideVideo,
+              setHideVideo,
+              seekTime,
+              setSeekTime,
+            }}
+          />
+        ) : null}
+        <hr />
+        {initialState ? (
+          <Editor
+            {...{ initialState, time, seekTo, speakers, setSpeakers, playing, play, pause }}
+            autoScroll
+            onChange={setDraft}
+            playheadDecorator={noKaraoke ? null : undefined}
+          />
+        ) : error ? (
+          <p>Error: {error?.message}</p>
+        ) : (
+          <p>{loading ? 'opening file / skeleton' : 'no file, please open one'}</p>
+        )}
+      </Container>
+    </Root>
   );
 };
 
