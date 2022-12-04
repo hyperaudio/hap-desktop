a5300587092766f19ab2c1191df0e7b868098032
piotrfonte <dev@piotrf.com>
2022-12-04 18:48:41 +0100

    Default colorAtom to deepPurple.

--- app/src/state/SettingsAtoms.ts
+++ app/src/state/SettingsAtoms.ts
@@ -1,8 +1,9 @@
 import { atomWithStorage } from 'jotai/utils';
 import { Color, PaletteMode } from '@mui/material';
+import { deepPurple } from '@mui/material/colors';
 
 // Get system theme
 export const darkMode = window.matchMedia('(prefers-color-scheme: dark)');
 
 export const settingsModeAtom = atomWithStorage<PaletteMode>('mode', darkMode ? 'dark' : 'light');
-export const settingsColorAtom = atomWithStorage<Color | undefined>('color', undefined);
+export const settingsColorAtom = atomWithStorage<Color>('color', deepPurple);
