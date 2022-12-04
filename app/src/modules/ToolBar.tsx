649d7677c51796b6fc19b650e6d20a812e5a397a
piotrfonte <dev@piotrf.com>
2022-12-04 18:47:52 +0100

    Add NewFile button to the toolbar.

--- app/src/modules/ToolBar.tsx
+++ app/src/modules/ToolBar.tsx
@@ -1,10 +1,11 @@
 import { ipcRenderer } from 'electron';
 import React, { useCallback } from 'react';
 
-import { IconButton, List, ListItem, Tooltip } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 import FileOpenIcon from '@mui/icons-material/FileOpen';
-import SettingsIcon from '@mui/icons-material/Settings';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { IconButton, List, ListItem, Tooltip } from '@mui/material';
 
 import { useSettingsDialog } from '@/modules';
 import { Stack } from '@mui/system';
@@ -21,7 +22,14 @@ export const ToolBar: React.FC<ToolBarProps> = ({ ...props }) => {
       <Stack direction="column" justifyContent="space-between" spacing={1} sx={{ height: '100%' }}>
         <List component={Stack} spacing={1}>
           <ListItem disablePadding sx={{ display: 'block', textAlign: 'center' }}>
-            <Tooltip arrow title="Open file…" placement="right">
+            <Tooltip arrow title="Create new…" placement="right">
+              <IconButton onClick={() => alert('Let me create a new file from here.')}>
+                <AddIcon />
+              </IconButton>
+            </Tooltip>
+          </ListItem>
+          <ListItem disablePadding sx={{ display: 'block', textAlign: 'center' }}>
+            <Tooltip arrow title="Open existing…" placement="right">
               <IconButton onClick={openFile}>
                 <FileOpenIcon />
               </IconButton>
