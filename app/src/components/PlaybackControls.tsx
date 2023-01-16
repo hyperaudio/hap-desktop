5785b7515f2f5e49f4816eb062f1b3c99942d92b
piotrfonte <dev@piotrf.com>
2023-01-16 19:34:34 +0100

    Add PlaybackSpeed dropdown to PlaybackControls.

--- app/src/components/PlaybackControls.tsx
+++ app/src/components/PlaybackControls.tsx
@@ -1,13 +1,16 @@
+import { useState } from 'react';
+
 import Box from '@mui/material/Box';
 import Forward10Icon from '@mui/icons-material/Forward10';
-import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
-import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import PauseIcon from '@mui/icons-material/Pause';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import Replay10Icon from '@mui/icons-material/Replay10';
 import Stack from '@mui/material/Stack';
-import { BoxProps, Typography } from '@mui/material';
+import Tooltip from '@mui/material/Tooltip';
+import { BoxProps } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 interface PlaybackControlsProps extends BoxProps {}
@@ -16,6 +19,7 @@ const PREFIX = 'PlaybackControls';
 const classes = {
   root: `${PREFIX}-root`,
   speedButton: `${PREFIX}-speedButton`,
+  menuButton: `${PREFIX}-menuButton`,
 };
 
 const Root = styled(Box)(({ theme }) => ({
@@ -29,9 +33,27 @@ const Root = styled(Box)(({ theme }) => ({
       marginRight: theme.spacing(0.33),
     },
   },
+  [`& .${classes.menuButton}`]: {
+    span: {
+      fontSize: '0.5rem',
+      display: 'inline-block',
+      marginRight: theme.spacing(0.33),
+    },
+  },
 }));
 
 export const PlaybackControls: React.FC<PlaybackControlsProps> = ({ ...props }) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [playing, setPlaying] = useState<boolean>();
+
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const onForward = () => {
     console.log('on rewind');
   };
@@ -39,33 +61,81 @@ export const PlaybackControls: React.FC<PlaybackControlsProps> = ({ ...props }) 
     console.log('on rewind');
   };
 
-  const isPlaying: boolean = false;
-
   return (
-    <Root className={classes.root}>
-      <Stack direction="row" alignItems="center">
-        <Tooltip title="Playback speed">
-          <IconButton size="small" className={classes.speedButton}>
-            <span>✕</span>1
-          </IconButton>
-        </Tooltip>
-        <Tooltip title={isPlaying ? 'Pause' : 'Play'}>
-          <IconButton size="large">
-            <PauseIcon fontSize="large" />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Rewind 10s">
-          <IconButton size="small" onClick={onRewind}>
-            <Replay10Icon />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Fast-forward 10s">
-          <IconButton size="small" onClick={onForward}>
-            <Forward10Icon />
-          </IconButton>
-        </Tooltip>
-      </Stack>
-    </Root>
+    <>
+      <Root className={classes.root}>
+        <Stack direction="row" alignItems="center">
+          <Tooltip title="Playback speed">
+            <IconButton
+              size="small"
+              className={classes.speedButton}
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+            >
+              <span>✕</span>1
+            </IconButton>
+          </Tooltip>
+          <Tooltip title={playing ? 'Pause' : 'Play'}>
+            <IconButton size="large" onClick={() => setPlaying(!playing)}>
+              {playing ? <PauseIcon fontSize="large" /> : <PlayArrowIcon fontSize="large" />}
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Rewind 10s">
+            <IconButton size="small" onClick={onRewind}>
+              <Replay10Icon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Fast-forward 10s">
+            <IconButton size="small" onClick={onForward}>
+              <Forward10Icon />
+            </IconButton>
+          </Tooltip>
+        </Stack>
+      </Root>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-label': 'Playback speed menu',
+          dense: true,
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Box component="span" sx={{ fontSize: '0.5rem', mr: 1 }}>
+            ✕
+          </Box>{' '}
+          0.5
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Box component="span" sx={{ fontSize: '0.5rem', mr: 1 }}>
+            ✕
+          </Box>{' '}
+          0.75
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Box component="span" sx={{ fontSize: '0.5rem', mr: 1 }}>
+            ✕
+          </Box>{' '}
+          1
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Box component="span" sx={{ fontSize: '0.5rem', mr: 1 }}>
+            ✕
+          </Box>{' '}
+          1.25
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Box component="span" sx={{ fontSize: '0.5rem', mr: 1 }}>
+            ✕
+          </Box>{' '}
+          1.5
+        </MenuItem>
+      </Menu>
+    </>
   );
 };
 
